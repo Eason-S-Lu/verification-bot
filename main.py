c7f7bd8e08520c65eb8eec4fc03d4d52cbe05545
Eason Lu <github@easonlu.ml>
2023-04-15 15:39:56 -0700

    Update main.py (#5)
    Signed-off-by: Eason Lu <github@easonlu.ml>

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ from discord.ext import commands
 import smtplib
 import random
 import os
+import logging
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 SMTP_USERNAME = os.getenv('SMTP_USERNAME')
 SMTP_PASSWORD = os.getenv('SMTP_PASSWORD')
-ALLOWED_DOMAINS = ['pausd.us', 'example.org'] # replace with the domains you want to allow
+ALLOWED_DOMAINS = ['pausd.us',] # replace with the domains you want to allow
 
-intents = discord.Intents.default()
-intents.members = True
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="~", intents=intents)
+
+logging.basicConfig(level=logging.DEBUG)
 
 def generate_verification_code():
     return str(random.randint(100000, 999999))
@@ -33,32 +37,62 @@ def send_verification_email(recipient_email, verification_code):
         server.sendmail(SMTP_USERNAME, recipient_email, message)
 
 async def send_verification_code(ctx):
-    verification_code = generate_verification_code()
-    send_verification_email(ctx.author.email, verification_code)
+    await ctx.send('Please enter your email address to receive a verification code.')
 
-    await ctx.send(f'Please enter the verification code sent to your email, {ctx.author.mention}.')
-
-    def check_verification_code(message):
-        return message.author == ctx.author and message.content == verification_code
+    def check_email(message):
+        return message.author == ctx.author and '@' in message.content and isinstance(message.channel, discord.TextChannel)
 
     try:
-        message = await client.wait_for('message', timeout=60.0, check=check_verification_code)
+        message = await bot.wait_for('message', timeout=60.0, check=check_email)
     except asyncio.TimeoutError:
         await ctx.send('Verification timed out. Please try again.')
     else:
-        role = discord.utils.get(ctx.guild.roles, name='Verified')
-        await ctx.author.add_roles(role)
-        await ctx.send('You have been verified!')
+        email = message.content.strip()
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+        await ctx.send('Please confirm your email address by typing it again.')
+
+        try:
+            message = await bot.wait_for('message', timeout=60.0, check=check_email)
+        except asyncio.TimeoutError:
+            await ctx.send('Verification timed out. Please try again.')
+        else:
+            confirm_email = message.content.strip()
+
+            if email != confirm_email:
+                await ctx.send("Email addresses don't match. Please start over.")
+                return
+
+            verification_code = generate_verification_code()
+            send_result = send_verification_email(email, verification_code)
+
+            if send_result is not None:
+                await ctx.send(send_result)
+            else:
+                logging.debug(f'Verification code {verification_code} sent to {email}.')
+                await ctx.send(f'Please enter the verification code sent to {email}.')
+
+                def check_verification_code(message):
+                    return message.author == ctx.author and message.content == verification_code and isinstance(message.channel, discord.TextChannel)
+
+                try:
+                    message = await bot.wait_for('message', timeout=60.0, check=check_verification_code)
+                except asyncio.TimeoutError:
+                    await ctx.send('Verification timed out. Please try again.')
+                else:
+                    guild = ctx.guild
+                    member = guild.get_member(ctx.author.id)
+                    role = discord.utils.get(guild.roles, name='Verified')
+                    await member.add_roles(role)
+                    await ctx.send('You have been verified!')
 
 @bot.event
 async def on_ready():
-    print('Bot is ready.')
+    logging.info('Bot is ready.')
 
 @bot.event
 async def on_member_join(member):
-    await member.send(f'Welcome to the server, {member.mention}! Please verify yourself by using the `!verify` command.')
+    user = member
+    await user.send(f'Welcome to the server, {user.mention}! Please verify yourself by using the `~verify` command.')
 
 @bot.command()
 async def verify(ctx):
